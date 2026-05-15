16f6222535c6259d1ecf09919baea42add7231a5
kreifeur <kreifeur669@gmail.com>
2026-05-15 16:00:45 +0100

    back to old key

--- src/app/membership/page.jsx
+++ src/app/membership/page.jsx
@@ -624,7 +624,7 @@ export default function Membership() {
                   {/* ReCAPTCHA */}
                   <div className="mb-8">
                     <div className="flex justify-center">
-                      <ReCAPTCHA ref={recaptchaRef} sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY || "6LdksessAAAAAEWu_z7-0t5cKQyt1MpuInZ-_AOx"} onChange={handleRecaptchaChange} />
+                      <ReCAPTCHA ref={recaptchaRef} sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY || "6LcVVlQsAAAAAAzjUdbl4n2fYmCTUsfPLKeppt_U"} onChange={handleRecaptchaChange} />
                     </div>
                   </div>
 
