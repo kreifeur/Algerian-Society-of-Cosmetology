e0bb03f7febd09748b4bc2ce8e083cf9d44fa0d9
kreifeur <kreifeur669@gmail.com>
2026-05-15 22:24:35 +0100

    remove id comande in failed

--- src/app/membership/failed/page.jsx
+++ src/app/membership/failed/page.jsx
@@ -124,7 +124,7 @@ function PaymentFailedContent() {
                 Une erreur est survenue lors de la vérification de votre
                 paiement.
               </p>
-              <p className="text-sm text-red-500 mb-6">{error}</p>
+             {/*  <p className="text-sm text-red-500 mb-6">{error}</p>
               {mdOrder && (
                 <div className="bg-gray-50 p-4 rounded mb-6">
                   <p className="text-sm text-gray-600">
@@ -132,7 +132,7 @@ function PaymentFailedContent() {
                     <span className="font-mono">{mdOrder}</span>
                   </p>
                 </div>
-              )}
+              )} */}
             </div>
             <div className="space-y-4">
               <div className="space-y-3">
@@ -174,7 +174,7 @@ function PaymentFailedContent() {
 
             <div className="mb-6">
               {/* Display transaction details if available */}
-              {mdOrder && (
+              {/* {mdOrder && (
                 <div className="bg-gray-50 p-4 rounded-lg mb-4 border border-gray-200">
                   <p className="text-sm font-medium text-gray-700 mb-1">
                     Référence de transaction :
@@ -182,7 +182,7 @@ function PaymentFailedContent() {
                   <p className="font-mono text-gray-800 text-sm">{mdOrder}</p>
                 </div>
               )}
-
+ */}
               {/* Display specific error message from API */}
               {(status?.ErrorCode && status.ErrorCode !== "0") ||
               (status?.errorCode && status.errorCode !== "0") ? (
@@ -294,7 +294,7 @@ function PaymentFailedContent() {
               </div>
             </div>
 
-            <div className="mt-6 pt-4 border-t border-gray-100 text-center">
+           {/*  <div className="mt-6 pt-4 border-t border-gray-100 text-center">
               <p className="text-xs text-gray-500">
                 Conservez la référence de transaction{" "}
                 <span className="font-mono text-gray-700">
@@ -302,7 +302,7 @@ function PaymentFailedContent() {
                 </span>{" "}
                 si vous contactez le support.
               </p>
-            </div>
+            </div> */}
           </>
         )}
       </div>
